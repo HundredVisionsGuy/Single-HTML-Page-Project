c4b0985cf42d6ce42a7148036c5a3790a8e9a017
Winikka <winikkc@hsd.k12.or.us>
2023-09-24 16:19:34 -0700

    adding reading level readability tests

--- tests/test_readability.py
+++ tests/test_readability.py
@@ -5,20 +5,23 @@ from file_clerk import clerk
 import pytest
 from webcode_tk import ux_tools
 
-@pytest.fixture
-def reading_level_goals():
-    goals = [("Min", 7),
-             ("Max", 11)]
-    return goals
+
+min_reading_level = 7
+max_reading_level = 11
+html_files = clerk.get_all_files_of_type("single_html_page/", "html")
+grade_level_expectations = []
+for file in html_files:
+    level = ux_tools.get_flesch_kincaid_grade_level(file)
+    grade_level_expectations.append((file, level, min_reading_level,
+                                     max_reading_level))
 
 
-@pytest.fixture
-def html_files():
-    files = clerk.get_all_files_of_type("single_html_page/", "html")
-    return files
+# @pytest.fixture
+# def html_files():
+#     files = clerk.get_all_files_of_type("single_html_page/", "html")
+#     return files
 
 
-@pytest.mark.parametrize("files",html_files)
-def test_files_for_readability_grade_level(reading_level_goals):
-    for file in html_files:
-        print("Nothing to see here...yet.")
+@pytest.mark.parametrize("file,actual,min,max", grade_level_expectations)
+def test_files_for_readability_grade_level(file, actual, min, max):
+    assert actual > min and actual < max
