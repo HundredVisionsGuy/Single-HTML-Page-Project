94ff41ba74c11163bbe8c584cae0179012ed2f9e
Hundredvisionsguy <cwinikka@gmail.com>
2023-10-02 18:08:28 -0700

    getting some color contrast tests in place

--- tests/test_css.py
+++ tests/test_css.py
@@ -1,74 +1,30 @@
 """
 Test CSS.
 """
-from file_clerk import clerk
 import pytest
 from webcode_tk import css_tools as css
+from webcode_tk import color_tools
 from webcode_tk import html_tools as html
 
 
 project_path = "single_html_page/"
-
-# # Get all styles (sheets and tags by file)
-# def get_styles_by_html_files(project_path):
-#     styles_by_html_files = []
-#     html_files = html.get_all_html_files(project_path)
-#     for file in html_files:
-#         file_data = css.get_all_stylesheets_by_file(file)
-#         styles_by_html_files.append({"file": file, "stylesheets": file_data})
-#     return styles_by_html_files
-
+html_files = html.get_all_html_files(project_path)
 styles_by_html_files = css.get_styles_by_html_files(project_path)
-
 global_color_rules = css.get_global_colors(project_path)
 
-print(global_color_rules)
-
-@pytest.fixture
-def html_style_tags():
-    html_files = html.get_all_html_files(project_path)
-    css_tags = []
-    for file in html_files:
-        style_tags = html.get_elements('style', file)
-        for tag in style_tags:
-            stylesheet = css.Stylesheet(file, tag.text, "style_tag")
-            css_tags.append(stylesheet)
-    return css_tags
+global_color_contrast_tests = []
+for file in html_files:
+    goal = "Normal AAA"
+    rule = global_color_rules.get(file)
+    global_color_contrast_tests.append((file, rule, goal, True))
 
 
-@pytest.fixture
-def all_stylesheets(html_style_tags):
-    stylesheets = html_style_tags
-    css_files = clerk.get_all_files_of_type(project_path, "css")
-    for file in css_files:
-        code = clerk.file_to_string(file)
-        sheet = css.Stylesheet(file, code)
-        stylesheets.append(sheet)
-    return stylesheets
-
-
-def test_css_for_font_pairing(all_stylesheets):
-    font_family_data = []
-    for sheet in all_stylesheets:
-        # Do something with tag
-        sheet_family_data = css.get_font_families(sheet)
-        for sheet_data in sheet_family_data:
-            selector = sheet_data.get("selector")
-            if sheet_family_data and not font_family_data:
-                font_family_data += sheet_family_data
-            elif sheet_family_data:
-                selectors = []
-                for item in sheet_family_data:
-                    print(item)
-                    
-
-
-    meets = (
-        len(font_family_data) == 2 and
-        font_family)
-    assert len(font_family_data) == 2
-
-
-# @pytest.fixture
-# def global_color_details(css_files):
-#     return css_files
+@pytest.mark.parametrize("file,rule,goal,expected",
+                         global_color_contrast_tests)
+def test_files_for_global_color_contrast(file, rule, goal, expected):
+    bg_color = rule.get("background-color")
+    bg_color = color_tools.get_hex(bg_color)
+    color = rule.get("color")
+    color = color_tools.get_hex(color)
+    result = color_tools.passes_color_contrast(goal, bg_color, color)
+    assert result == expected
